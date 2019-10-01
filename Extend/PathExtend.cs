df8ea62a767c83df05b6fa7fdc9784fa44225f54
shlifedev <shlifedev@gmail.com>
2019-10-01 13:45:52 +0900

    update

---
+++ Extend/PathExtend.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 경로 설정용.
+/// </summary>
+public static class PathExtend
+{
+    /// <summary>
+    /// A/B/C.prefab => C:\project\Assets/A/B/C.prefab
+    /// </summary>
+    /// <param name="assetPath"></param>
+    /// <returns></returns>
+    public static string GetFullPath(string assetPath)
+    {
+        assetPath = assetPath.Replace(@"\", "/"); 
+        assetPath = UnityEngine.Application.dataPath.Replace(@"\", "/") + $"/{assetPath}";
+        return assetPath;
+    }
+
+    /// <summary>
+    /// c:\project/Assets/A/B => Assets/A/B
+    /// </summary>
+    /// <param name="fullPath"></param>
+    /// <returns></returns>
+    public static string FullPathToPorjectPath(string fullPath)
+    {
+        fullPath = fullPath.Replace(@"\", "/");
+        var assetPath = UnityEngine.Application.dataPath.Replace(@"\", "/") + "/";
+        fullPath = fullPath.Replace(assetPath, null);
+        return $"Assets/{fullPath}";
+    }
+}
---
+++ Extend/VectorExtend.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Vector 관련 익스텐더
+/// </summary>
+public static class VectorExtend
+{
+    public static Vector2 Vector3ToXZ(this Vector3 p)
+    {
+        return new Vector2(p.x, p.z);
+    }
+    public static Vector2 Vector3ToYZ(this Vector3 p)
+    {
+        return new Vector2(p.y, p.z);
+    }
+    public static Vector2 Vector3ToZY(this Vector3 p)
+    {
+        return new Vector2(p.z, p.y);
+    }
+    public static Vector2 Vector3ToXY(this Vector3 p)
+    {
+        return new Vector2(p.x, p.y);
+    }
+    public static Vector2 Vector3ToYX(this Vector3 p)
+    {
+        return new Vector2(p.y, p.x);
+    }
+    public static Vector3 Vector2ToXYZ(this Vector3 p, float y = 0)
+    {
+        return new Vector3(p.x, y, p.z);
+    }
+    public static float[] ToFloat3Array(this Vector3 p)
+    {
+        return new float[] { p.x, p.y, p.z };
+    }
+    public static Vector3 ToVector3(this float[] p)
+    {
+        if (p.Length == 3)
+        {
+            return new Vector3(p[0], p[1], p[2]);
+        }
+        else
+            return Vector3.zero;
+    }
+
+}
